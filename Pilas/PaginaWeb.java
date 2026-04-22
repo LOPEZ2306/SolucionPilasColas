80d9ce11381252ca9173dc0e6c5924df9e92673b
LOPEZ2306 <juanmalopvel@gmail.com>
2026-04-22 18:03:13 -0500

    Solucion ejercicio pilas

---
+++ Pilas/PaginaWeb.java
@@ -0,0 +1,30 @@
+public class PaginaWeb {
+    private String url;
+    private String titulo;
+    private int horaAcceso;
+
+    public PaginaWeb(String url, String titulo, int horaAcceso) {
+        this.url = url;
+        this.titulo = titulo;
+        this.horaAcceso = horaAcceso;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getHoraAcceso() {
+        return horaAcceso;
+    }
+
+    @Override
+    public String toString() {
+        return "Titulo: " + titulo +
+               " | URL: " + url +
+               " | Hora Acceso: " + horaAcceso;
+    }
+}
